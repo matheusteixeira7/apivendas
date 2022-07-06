56ac55e7fef0dfa78b02489f49a9fea2ed7918a3
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-06 14:47:31 -0300

    chore: project config

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+
+export const routes = Router()
+
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello World' })
+})
---
+++ src/shared/http/server.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata'
+import { AppError } from '@shared/errors/AppError'
+import cors from 'cors'
+import express, { NextFunction, Request, Response } from 'express'
+import { routes } from './routes'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333')
+})
