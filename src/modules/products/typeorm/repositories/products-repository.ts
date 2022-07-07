971ea6336c95e82fc9e073e18f67c7c53793e951
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-07 12:38:18 -0300

    feat: add ProductsRepository

---
+++ src/modules/products/typeorm/repositories/products-repository.ts
@@ -0,0 +1,13 @@
+import { EntityRepository, Repository } from 'typeorm'
+import { Product } from '../entities'
+
+@EntityRepository(Product)
+export class ProductsRepository extends Repository<Product> {
+  async findByName (name: string): Promise<Product | undefined> {
+    const product = await this.findOne({
+      where: { name }
+    })
+
+    return product
+  }
+}
