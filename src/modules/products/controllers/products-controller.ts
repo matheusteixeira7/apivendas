86f7f4d1450f47a7cbeef7552dd93f264d742240
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-07 16:22:07 -0300

    fix: change product service instance to inside a method

--- src/modules/products/controllers/products-controller.ts
+++ src/modules/products/controllers/products-controller.ts
@@ -2,47 +2,50 @@ import { Request, Response } from 'express'
 import { CreateProductService, DeleteProductService, GetAllProductsService, GetProductService, UpdateProductService } from '../services'
 
 export class ProductsController {
-  private getAllProducts = new GetAllProductsService()
-  private getProduct = new GetProductService()
-  private createProduct = new CreateProductService()
-  private updateProduct = new UpdateProductService()
-  private deleteProduct = new DeleteProductService()
-
   async getAll (request: Request, response: Response): Promise<Response> {
-    const products = await this.getAllProducts.execute()
+    const getAllProducts = new GetAllProductsService()
+    const products = await getAllProducts.execute()
 
     return response.status(200).json(products)
   }
 
   async getById (request: Request, response: Response): Promise<Response> {
+    const getByIdProduct = new GetProductService()
+
     const { id } = request.params
 
-    const product = await this.getProduct.execute({ id })
+    const product = await getByIdProduct.execute({ id })
 
     return response.status(200).json(product)
   }
 
   async create (request: Request, response: Response): Promise<Response> {
+    const createProduct = new CreateProductService()
+
     const { name, price, quantity } = request.body
 
-    const product = await this.createProduct.execute({ name, price, quantity })
+    const product = await createProduct.execute({ name, price, quantity })
 
     return response.status(201).json(product)
   }
 
   async update (request: Request, response: Response): Promise<Response> {
+    const updateProduct = new UpdateProductService()
+
     const { id } = request.params
     const { name, price, quantity } = request.body
 
-    const product = await this.updateProduct.execute({ id, name, price, quantity })
+    const product = await updateProduct.execute({ id, name, price, quantity })
 
     return response.status(201).json(product)
   }
 
   async delete (request: Request, response: Response): Promise<Response> {
+    const deleteProduct = new DeleteProductService()
+
     const { id } = request.params
 
-    await this.deleteProduct.execute({ id })
+    await deleteProduct.execute({ id })
 
     return response.status(204).send()
   }
