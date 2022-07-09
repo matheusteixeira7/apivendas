1bbb247ccb3c2271dd37acaf8ff23883f3d92e7d
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-09 19:43:02 -0300

    feat: add JWT

--- src/modules/users/services/create-session-service.ts
+++ src/modules/users/services/create-session-service.ts
@@ -1,5 +1,6 @@
 import { AppError } from '@shared/errors/AppError'
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 import { getCustomRepository } from 'typeorm'
 import { User } from '../typeorm/entities'
 import { UsersRepository } from './../typeorm/repositories/users-repository'
@@ -9,8 +10,13 @@ interface IRequest {
   password: string
 }
 
+interface IResponse {
+  user: User
+  token: string
+}
+
 export class CreateSessionService {
-  async execute ({ email, password }: IRequest): Promise<User> {
+  async execute ({ email, password }: IRequest): Promise<IResponse> {
     const userRepository = getCustomRepository(UsersRepository)
 
     const user = await userRepository.findByEmail(email)
@@ -25,6 +31,11 @@ export class CreateSessionService {
       throw new AppError('Incorrect email/password combination', 401)
     }
 
-    return user
+    const token = sign({}, '79dd44c92406bd7075f08987975c3010', {
+      subject: user.id,
+      expiresIn: '1d'
+    })
+
+    return { user, token }
   }
 }
