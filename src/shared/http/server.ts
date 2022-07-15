6789abb74531bffc23881f6eafbfb0befe187ce6
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-15 12:25:46 -0300

    feat: add user_tokens migration

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata'
 
 import express, { NextFunction, Request, Response } from 'express'
 
+import uploadConfig from '@config/upload'
 import { AppError } from '@shared/errors/AppError'
 import '@shared/typeorm'
 import { errors } from 'celebrate'
@@ -13,6 +14,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('./files', express.static(uploadConfig.directory))
 app.use(routes)
 app.use(errors())
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
