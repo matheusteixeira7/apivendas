375b1fe877c5e1616028d1029052ae5997fb83d2
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-18 11:40:15 -0300

    feat: add UpdateUserService

---
+++ src/modules/users/controllers/profile-controller.ts
@@ -0,0 +1,42 @@
+import { AppError } from '@shared/errors/AppError'
+import { Request, Response } from 'express'
+import { GetUserService } from '../services/get-user-service'
+import { UpdateUserService } from '../services/update-user-service'
+
+export class ProfileController {
+  async get (request: Request, response: Response): Promise<Response> {
+    const getUser = new GetUserService()
+
+    if (!request.user) {
+      throw new AppError('User not found')
+    }
+
+    const userId = request.user.id
+
+    const user = await getUser.execute({ userId })
+
+    return response.status(200).json(user)
+  }
+
+  async update (request: Request, response: Response): Promise<Response> {
+    const { name, email, password, oldPassword } = request.body
+
+    if (!request.user) {
+      throw new AppError('User not found')
+    }
+
+    const userId = request.user.id
+
+    const updateUser = new UpdateUserService()
+
+    const user = await updateUser.execute({
+      userId,
+      name,
+      email,
+      password,
+      oldPassword
+    })
+
+    return response.status(200).json(user)
+  }
+}
