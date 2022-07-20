4cc9594aa88bc7b51f26868509257c43ec766885
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-07-20 18:31:51 -0300

    feat: add OrdersController and orders routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import { customersRouter } from '@modules/customers/routes'
+import { ordersRouter } from '@modules/orders/routes'
 import { productsRouter } from '@modules/products/routes'
 import { passwordRouter, profileRouter, sessionRouter, usersRouter } from '@modules/users/routes'
 import { Router } from 'express'
@@ -11,3 +12,4 @@ routes.use('/session', sessionRouter)
 routes.use('/password', passwordRouter)
 routes.use('/profile', profileRouter)
 routes.use('/customers', customersRouter)
+routes.use('/orders', ordersRouter)
